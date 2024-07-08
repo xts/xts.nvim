20c537fe7a8869668a07536f83f4ca8b49ef031e
Terje Stoeback <tstoeback@bloomberg.net>
2024-07-08 10:14:41 -0400

    Add line number to github url.

--- lua/xts/github.lua
+++ lua/xts/github.lua
@@ -8,13 +8,14 @@ local find_remote_url = function(name)
 
 	local url = handle:read("*l")
 	handle:close()
-	return url
+	return url and string.gsub(url, "ssh://", "https://")
 end
 
 M.find_github_url = function()
 	local base_url = find_remote_url("upstream") or find_remote_url("origin")
 	local file = vim.fn.expand("%:.")
-	local url = base_url .. "/blob/main/" .. file
+	local line = vim.api.nvim_win_get_cursor(0)[1]
+	local url = base_url .. "/blob/main/" .. file .. "#L" .. line
 	return url
 end
 
